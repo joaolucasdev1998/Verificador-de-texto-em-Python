7d6014d0463c1cfedba20bfef6bc227cce736e97
joaolucasdev1998 <joaolucascanger@gmail.com>
2026-01-11 14:13:06 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,27 @@
+nome = input("Digite seu nome")
+encontrar = input("Digite o que deseja encontrar:")
+if encontrar in nome:
+ print (f'{encontrar}está em{nome}')
+else:
+ print(f'{encontrar}nao está em{nome}')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
